99dc0b06595e448cca4ed733ebeb2417e0eecad2
Akira794 <s1426079kv@s.chibakoudai.jp>
2018-06-26 11:34:40 +0900

    修正

--- observer.py
+++ observer.py
@@ -22,23 +22,6 @@ if ck.check_obsv(sys.A, sys.C) == -1:
      print("Not Observability\n")
      exit
 
-f = place(A.T, C.T,[-3, -4]).T
+L = place(A.T, C.T,[-3, -4]).T
 
-print("Observer_Gain:\n",f)
-
-sys_fb = ss(sys.A - sys.B *f, sys.B, sys.C, sys.D)
-
-N = 1024
-x0 = [1.0, 0.0]
-t = np.linspace(0, 7, 1024)
-u = np.zeros(N)
-
-out, t_b, x_b = lsim(sys, U=u, T=t, X0=x0)
-out_fb, t_fb, x_fb = lsim(sys_fb, U=u, T=t, X0=x0)
-plt.plot(t_fb, out_fb[:,1], label="output")
-plt.plot(t_b, x_b[:,0], label="$X_1$")
-plt.plot(t_b, x_b[:,1], label="$X_2$")
-plt.plot(t_fb, x_fb[:,0], label="$X_1 feed back$")
-plt.plot(t_fb, x_fb[:,1], label="$X_2 feed back$")
-plt.legend()
-plt.show()
+print("Observer_Gain:\n",L)
